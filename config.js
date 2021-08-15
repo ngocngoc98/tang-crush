2283cf8f1b1d5f340584dc003650bb43aae0ccd9
ngocngoc98 <88965749+ngocngoc98@users.noreply.github.com>
2021-08-15 16:28:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Anh Bếu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là iu em <33',
+    btnNo: 'Không, em iu à :3',
+    question:'Anh có muốn đưa Pass fb,zalo,gmail..cho em,mún e làm nóc nhà cụa anh? <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yebbbb...Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'En biết mà 🥰. Yêu anh Bếu nhiều nhiều 😘😘',
+    messDesc: 'Hứa nhé.Hết dịch mình đi ăn lẩu nhé :v .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/gem.ngoc.94'
 }
